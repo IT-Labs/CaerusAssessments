31a6c0ba9d3b243a39b22436533a9c313a802eb0
Blagoj Janev <blagoj.janev@it-labs.com>
2018-04-25 14:21:20 +0200

    truffle initial commit

---
+++ src/truffle.js
@@ -0,0 +1,28 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "century shuffle favorite seven venture code thing elephant cannon cross shuffle glare";
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // eslint-disable-line camelcase
+    },
+    ropsten: { 
+      provider: function() { 
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/IyFnJGhaYQV2dRHD7EkW", 0) 
+      }, 
+      network_id: 3, 
+      //from: "0xC2fdd99ce48D5e0191c1d77d17f88aa61370033F" 
+      gasPrice: 9000000000,
+      gas: 4500000
+    }, 
+  } 
+};
