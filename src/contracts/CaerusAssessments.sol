a972771f3bd85ef9bbb2002b55fb84f6c82246be
Blagoj Janev <blagoj.janev@it-labs.com>
2018-06-11 09:42:24 +0200

    Adding event

--- src/contracts/CaerusAssessments.sol
+++ src/contracts/CaerusAssessments.sol
@@ -5,9 +5,13 @@ import "./CaerusBase.sol";
 
 
 contract CaerusAssessments is CaerusBase {
-
-    function storeAssessment(string _userId, string _assessmentId, string _result) external onlyAllowedUsers returns (bool) {
+    event SetAssessmentEvent(
+        string indexed _userId,
+        string indexed _assessmentId
+    );
+    function setAssessment(string _userId, string _assessmentId, string _result) external onlyAllowedUsers returns (bool) {
         caerusStorage.setString(keccak256("assessment.", _userId, _assessmentId), _result);
+        emit SetAssessmentEvent(_userId, _assessmentId);
         return true;
     }
 
